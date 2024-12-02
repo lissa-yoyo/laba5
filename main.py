7a43f6eb3a3af7f62e26328367abd0ba2f4cc6ec
lissa-yoyo <alisaalecseeva@gmail.com>
2024-12-02 11:11:12 +0300

    2 commit

--- main.py
+++ main.py
@@ -6,3 +6,11 @@ else:
     res = set(string1) - set(string2)
     print("Символы, которые встречаются в первой строке, но не встречаюся во второй:")
     print(*res)
+
+res1 = set(string1) & set(string2)
+if len(res1) == 0:
+    print("Совпадений не найдено")
+else:
+    res1 = sorted(res1)
+    print("Числа, которые встречаются в обоих наборах чисел: ")
+    print(*res1)
